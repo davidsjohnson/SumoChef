3946a8cf89ffe30d1cc802a58145f415117ca7d4
David Johnson <fortjay81@gmail.com>
2013-03-18 02:08:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SumochefApp::Application.config.secret_token = '6057537ce8a79eac66cc3a6430b321fef5d48669a5c5834af1da2f4587b6ad53520d91aba2c83718e918e9b96bd80918d08f85e86548dcf8d1dbc95b34a911f9'
