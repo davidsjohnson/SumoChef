a57f0ac004c84c6c61d2b69a0953e4adbfd10857
David Johnson <fortjay81@gmail.com>
2013-03-23 11:46:04 -0400

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SumochefApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/sign_in"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
