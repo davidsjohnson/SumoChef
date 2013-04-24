fbf4a2fd888235e2eb64e33beba37ae33e27ca26
David Johnson <fortjay81@gmail.com>
2013-04-23 23:43:17 -0400

    formatting changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,18 @@ class StaticPagesController < ApplicationController
   
   def index
   	 @recipes = Recipe.search(params[:search]).paginate(page: params[:page], per_page: 20)
+  	 @user = current_user
+  	if signed_in?
+  		@shopping_lists = @user.shopping_lists.find_all_by_state("active").paginate(page: params[:list_page], per_page: 3)
+  	end
   end
 
   def home
   	@recipes = Recipe.paginate(page: params[:page], per_page: 20)
+  	@user = current_user
+  	if signed_in?
+  		@shopping_lists = @user.shopping_lists.find_all_by_state("active").paginate(page: params[:list_page], per_page: 3)
+  	end
   end
 
   def help
